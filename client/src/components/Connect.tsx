44fbf6f3b59d102e37b34637e9854b81d75380b3
David Huang <huangd1224@gmail.com>
2022-10-31 01:05:46 -0400

    Draft done

--- client/src/components/Connect.tsx
+++ client/src/components/Connect.tsx
@@ -1,10 +1,11 @@
 import { useState } from 'react'
 
-export default function Connect(props: {connect: Function}) {
+export default function Connect(props: {connect: Function, connecting: Boolean}) {
     const [id, setid] = useState('');
 
     return (
-        <form onSubmit={() => props.connect(id)}>
+        props.connecting ? <div>Connecting...</div> :
+        <form onSubmit={(e) => {e.preventDefault(); props.connect(id)}}>
             <div>Document ID: </div>
             <input id='document-id' name='document-id' type='text' value={id} onChange={(e) => setid(e.target.value)}/>
             <button id='connect-btn' type='submit'>Open</button>
